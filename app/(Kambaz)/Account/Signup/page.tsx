162874f4a54d063e15b452e688782dbb6a842252
Sruthi Annavarapu <sruthiannavarapu@Sruthis-MacBook-Air.local>
2025-09-22 19:10:02 -0400

    third

--- app/(Kambaz)/Account/Signup/page.tsx
+++ app/(Kambaz)/Account/Signup/page.tsx
@@ -3,10 +3,9 @@ export default function Signup() {
   return (
     <div id="wd-signup-screen">
       <h3>Sign up</h3>
-      <input placeholder="username" className="wd-username" /><br/>
-      <input placeholder="password" type="password" className="wd-password" /><br/>
-      <input placeholder="verify password"
-             type="password" className="wd-password-verify" /><br/>
+      <input placeholder="username" defaultValue="alice" className="wd-username" /><br/>
+      <input placeholder="password" defaultValue="Alice@123" type="password" className="wd-password" /><br/>
+      <input placeholder="verify password" defaultValue="Alice@123" type="password" className="wd-password-verify" /><br/>
       <Link  href="Profile" > Sign up </Link><br />
       <Link  href="Signin" > Sign in </Link>
     </div>
