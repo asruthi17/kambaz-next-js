6830c40d60abc5082760081279500c91ce6f6b68
asruthi17 <151494070+asruthi17@users.noreply.github.com>
2025-10-06 20:17:23 -0400

    Navigation.tsx

--- app/(Kambaz)/Account/Navigation.tsx
+++ app/(Kambaz)/Account/Navigation.tsx
@@ -1,23 +1,42 @@
+"use client";
+import { usePathname } from "next/navigation";
 import Link from "next/link";
 
 export default function AccountNavigation() {
+  const pathname = usePathname();
+  
   return (
     <div id="wd-account-navigation" className="list-group" style={{ width: "200px" }}>
       <Link 
         href="/Account/Signin" 
-        className="list-group-item list-group-item-action text-danger border-0"
+        className={`list-group-item list-group-item-action border-0 ${
+          pathname === "/Account/Signin" 
+            ? "border-start border-dark border-4 text-dark fw-bold" 
+            : "text-danger"
+        }`}
+        style={{ backgroundColor: 'transparent' }}
       >
         Signin
       </Link>
       <Link 
         href="/Account/Signup" 
-        className="list-group-item list-group-item-action text-danger border-0"
+        className={`list-group-item list-group-item-action border-0 ${
+          pathname === "/Account/Signup" 
+            ? "border-start border-dark border-4 text-dark fw-bold" 
+            : "text-danger"
+        }`}
+        style={{ backgroundColor: 'transparent' }}
       >
         Signup
       </Link>
       <Link 
         href="/Account/Profile" 
-        className="list-group-item list-group-item-action text-danger border-0"
+        className={`list-group-item list-group-item-action border-0 ${
+          pathname === "/Account/Profile" 
+            ? "border-start border-dark border-4 text-dark fw-bold" 
+            : "text-danger"
+        }`}
+        style={{ backgroundColor: 'transparent' }}
       >
         Profile
       </Link>
