aad30f6a26d415edf66550b5abdd6a79abd441fa
Sruthi Annavarapu <sruthiannavarapu@Sruthis-MacBook-Air.local>
2025-10-26 21:09:17 -0400

    First

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,9 @@ const nextConfig: NextConfig = {
   images: {
     domains: ["www.staradvertiser.com"], // add external image hostname here
   },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
