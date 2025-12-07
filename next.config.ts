8ba03ba4362f3e11ccf1e08da554350a65a1e032
Sruthi Annavarapu <sruthiannavarapu@Sruthis-MacBook-Air.local>
2025-12-07 15:43:36 -0500

    Final

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,13 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
+const nextConfig: NextConfig & {
+  eslint?: {
+    ignoreDuringBuilds?: boolean;
+  };
+} = {
   reactStrictMode: true,
   images: {
-    domains: ["www.staradvertiser.com"], 
+    domains: ["www.staradvertiser.com"],
   },
   eslint: {
     ignoreDuringBuilds: true,
